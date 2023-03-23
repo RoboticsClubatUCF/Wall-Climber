abd31760651df857273f5ec7fb5c8b80a5cbfd9f
Quinn Barber <87210541+Quinn-Barber@users.noreply.github.com>
2023-03-23 19:47:38 -0400

    Updated imaging file, and maze images
    This is a much more extensive imaging file than previously used:
    
    * Functions and comments are utilized to make it more clearly understandable
    * We now can differentiate between the start and end coordinates
    * Prepped file for transferring pixel to inch commands and send movement commands to robot using inches

--- src/Raspberry_pi/computer_vision/imaging.py
+++ src/Raspberry_pi/computer_vision/imaging.py
@@ -1,202 +1,318 @@
-import matplotlib.pylab as plt
-from skimage.morphology import skeletonize, thin
+
+# Imaging python file, the purpose of this file is to take and process
+# the image that we are using for the maze with the robot on it.
+
+# We then will find the shortest path from where the robots start point is
+# to where the robots end point is, using this we send directional commands
+# to the robot in order to move it to this destination!
+
+# Author: Quinn Barber
+
+from skimage.morphology import skeletonize
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
-from skimage.util import invert
-
-# Change file name accordingly to your image
-img_name = 'maze6.jpg'
-
-rgb_img = cv.imread(img_name)
-
-# Copy the image over so that we can mask for the start and end points
-find_points_img = rgb_img
-hsv_points = cv.cvtColor(find_points_img, cv.COLOR_BGR2HSV)
-
-lower_green = np.array([40, 40, 40])
-upper_green = np.array([70, 255, 255])
-
-points_mask = cv.inRange(hsv_points, lower_green, upper_green)
-
-# Here is the original mask of the points, uncomment line below to save this image
-# cv.imwrite("maskPoints.jpg", points_mask)
-
-# This mask isn't perfect, so we must find the contours and remove the smaller bits and pieces that got through the mask
-contours, hierarchy = cv.findContours(points_mask, mode = cv.RETR_LIST, method = cv.CHAIN_APPROX_SIMPLE)
-
-# Create this empty image of the same size
-newMask = np.zeros(rgb_img.shape[:2], dtype=np.uint8)
-
-# Now we go through all contours, and only add the start and end nodes (this is if the contour has a larger area)
-# The bits and pieces will not be drawn onto this new mask
-for i, cnt in enumerate(contours):
-    if hierarchy[0][i][2] == -1:
-        if cv.contourArea(cnt) > 1000:
-            cv.drawContours(newMask, [cnt], 0, (255), -1)
+import math
+import sys
 
 
-# Here is the finished mask of the points, uncomment line below to save this image
-# cv.imwrite("newMask.jpg", newMask)
+def importImage(image_name):
 
-# Now we must find the center of the start and end points based off of this mask
-# We can do this with connected components in openCV using the line below
-output = cv.connectedComponentsWithStats(newMask, 4, cv.CV_32S)
-centroids = output[3]
-stats = output[2]
+    # Read in the image as an image file using CV
+    originalImage = cv.imread(image_name)
 
-# The centroids are the centers of every connected component
-# However, this includes all black pixels being connected (which aren't the points)
-# We can find if the area is too large using the stats of the components
-# We can then find the component of the black pixels and delete them
-for i in range(len(stats)):
-    if stats[i][4] > 50000:
-        centroids = np.delete(centroids, i, axis=0)
+    # Get hsv points of the image
+    hsv_points = cv.cvtColor(originalImage, cv.COLOR_BGR2HSV)
 
-# This leaves us with only the centroid of the start and end nodes
-# Ta-da!
-x0, y0 = int(centroids[0][0]), int(centroids[0][1])
-x1, y1 = int(centroids[1][0]), int(centroids[1][1])
+    # Add these values to an array to return
+    imageArray = []
+    imageArray.append(originalImage)
+    imageArray.append(hsv_points)
 
-print("Start and End Nodes found at the coordinates below\n")
-print("Start: x = ", x0, " y = ", y0, "\n")
-print("End: x = ", x1, " y = ", y1, "\n")
+    return imageArray
 
-# Let us now go back to our original image and mask for the walls of the maze
-# We do this given the lowest and highest blue values of the walls
-low_blue = np.array([50, 50, 50])
-high_blue = np.array([255, 255, 255])
+def createMasks(hsv_points, originalImage):
 
-hsv = cv.cvtColor(rgb_img, cv.COLOR_BGR2HSV)
-mask = cv.inRange(hsv, low_blue, high_blue)
+    # Masks are created using the inRange function in CV with lower and upper values
 
-# This is the original mask of the walls, uncomment the line below to save this image
-# cv.imwrite("mask.jpg", mask)
+    # Mask for green
+    lower_green = np.array([0, 100, 50])
+    upper_green = np.array([80, 255, 140])
 
-# We now do the same process of getting rid of the small bits and pieces using contours
-contours, hierarchy = cv.findContours(mask, mode = cv.RETR_LIST, method = cv.CHAIN_APPROX_SIMPLE)
+    # Mask for red
+    lower_red = np.array([0, 180,20])
+    upper_red = np.array([10, 255, 255])
 
-# This is the empty image
-newMask = np.zeros(rgb_img.shape[:2], dtype=np.uint8)
+    # Mask for blue
+    lower_blue = np.array([100, 0, 0])
+    upper_blue = np.array([255, 255, 95])
 
-# We find the largest and second-largest contours
-# In this case, the second-largest contours is the total area inside the walls of the maze
-# While the largest contour is the area of the entire maze itself
-largest = contours[0]
-secondLargest = contours[0]
+    # Mask for orange
+    lower_orange = np.array([0, 100, 150])
+    upper_orange = np.array([80, 255, 255])
 
-# Go through all the contours and find this largest and second-largest value
-for i, cnt in enumerate(contours):
-    if hierarchy[0][i][2] == -1:
-        if cv.contourArea(cnt) > cv.contourArea(largest):
-            secondLargest = largest
-            largest = cnt
+    # Creating the actual masks
+    green_mask = cv.inRange(originalImage, lower_green, upper_green)
+    red_mask = cv.inRange(hsv_points, lower_red, upper_red)
+    blue_mask = cv.inRange(originalImage, lower_blue, upper_blue)
+    orange_mask = cv.inRange(originalImage, lower_orange, upper_orange)
 
-# Draw the second largest to have the total area inside the maze
-cv.drawContours(newMask, [secondLargest], 0, (255), -1)
+    # Set them into a dictionary and return them
+    mask_dict = {'green': green_mask, 'red': red_mask, 'blue': blue_mask, 'orange': orange_mask}
 
-# This is the end mask of the area inside the maze, uncomment the line below to save this image
-# cv.imwrite("afterMask.jpg", newMask)
+    return mask_dict
 
-# Save this new mask
-mask = newMask
+def contourMasks(mask_dict, originalImage):
 
-# Go through and threshold for the black and white of this mask
-if mask.shape.__len__() > 2:
-    thr_img = (mask[:, :, 0] > np.max(mask[:, :, 0])/2)
-else:
-    thr_img = mask > np.max(mask)/2
+    # Removes noise from the masks (green, blue, and orange)
 
-# Skeletonize the inside of this
-skeleton = skeletonize(thr_img)
+    # Find all contours of the green mask and initialize empty image file to draw updated mask into (updatedGreenMask)
+    contours, hierarchy = cv.findContours(mask_dict['green'], mode=cv.RETR_LIST, method=cv.CHAIN_APPROX_SIMPLE)
+    updatedGreenMask = np.zeros(originalImage.shape[:2], dtype=np.uint8)
 
-# map of routes.
-mapT = ~skeleton
+    # Add only the contours we want into updatedGreenMask
+    for i, contour in enumerate(contours):
+        if hierarchy[0][i][2] == -1:
+            if cv.contourArea(contour) > 1000:
+                cv.drawContours(updatedGreenMask, [contour], 0, 255, -1)
 
-_mapt = np.copy(mapT)
+    # Update the mask stored in the dictionary
+    mask_dict['green'] = updatedGreenMask
 
-# searching for our end point and connect to the path.
-# THIS VALUE MAY HAVE TO BE CHANGED
-# IT IS THE RADIUS THAT YOU ARE ALLOWING FROM THE PATH TO THE START/END
-# THIS COULD GLITCH OUT IF THE AREA INSIDE THE WALLS BY THE START/END IS TOO LARGE
-# I.E. NO WALLS WITHIN RANGE OF START AND END
-boxr = 30
+    # Find contours of the blue mask and initialize empty image file to draw updated mask into (updatedBlueMask)
+    contours, hierarchy = cv.findContours(mask_dict['blue'], mode=cv.RETR_LIST, method=cv.CHAIN_APPROX_SIMPLE)
+    updatedBlueMask = np.zeros(originalImage.shape[:2], dtype=np.uint8)
 
-# Just a little safety check, if the points are too near the edge, it will error.
-if y1 < boxr: y1 = boxr
-if x1 < boxr: x1 = boxr
+    # For the blue mask, we are looking for the second-largest contour to add to our
+    # new mask, this in every case is the area that is contained within the walls of
+    # the maze, this is what we are running skeletonize on to find possible paths
+    # throughout the maze
 
-cpys, cpxs = np.where(_mapt[y1 - boxr:y1 + boxr, x1 - boxr:x1 + boxr] == 0)
-# calibrate points to main scale.
-cpys += y1 - boxr
-cpxs += x1 - boxr
+    # To do this we initialize the largest and secondLargest to the first in the array
+    largest = contours[0]
+    secondLargest = contours[0]
 
-# find the closest point of possible path end points
-idx = np.argmin(np.sqrt((cpys - y1) ** 2 + (cpxs - x1) ** 2))
-y, x = cpys[idx], cpxs[idx]
+    # Update the values as we loop through the contours
+    for i, contour in enumerate(contours):
+        if hierarchy[0][i][2] == -1:
+            if cv.contourArea(contour) > cv.contourArea(largest):
+                secondLargest = largest
+                largest = contour
 
-pts_x = [x]
-pts_y = [y]
-pts_c = [0]
+    cv.drawContours(updatedBlueMask, [secondLargest], 0, 255, -1)
 
-# mesh of displacements.
-xmesh, ymesh = np.meshgrid(np.arange(-1, 2), np.arange(-1, 2))
-ymesh = ymesh.reshape(-1)
-xmesh = xmesh.reshape(-1)
+    # Update the mask stored in the dictionary
+    mask_dict['blue'] = updatedBlueMask
 
-dst = np.zeros(thr_img.shape)
+    # We will now remove noise from the orange mask using the same method we used for
+    # the green mask
+    contours, hierarchy = cv.findContours(mask_dict['orange'], mode=cv.RETR_LIST, method=cv.CHAIN_APPROX_SIMPLE)
+    updatedOrangeMask = np.zeros(originalImage.shape[:2], dtype=np.uint8)
 
-# Breath first algorithm exploring a tree
-while True:
-    # update distance.
-    idc = np.argmin(pts_c)
-    ct = pts_c.pop(idc)
-    x = pts_x.pop(idc)
-    y = pts_y.pop(idc)
-    # Search 3x3 neighbourhood for possible
-    ys, xs = np.where(_mapt[y - 1:y + 2, x - 1:x + 2] == 0)
-    # Invalidate these point from future searchers.
-    _mapt[ys + y - 1, xs + x - 1] = ct
-    _mapt[y, x] = 9999999
-    # set the distance in the distance image.
-    dst[ys + y - 1, xs + x - 1] = ct + 1
-    # extend our list.s
-    pts_x.extend(xs + x - 1)
-    pts_y.extend(ys + y - 1)
-    pts_c.extend([ct + 1] * xs.shape[0])
-    # If we run of points.
-    if not pts_x:
-        break
-    if np.sqrt((x - x0) ** 2 + (y - y0) ** 2) < boxr:
-        edx = x
-        edy = y
-        break
+    for i, contour in enumerate(contours):
+        if hierarchy[0][1][2] == -1:
+            if cv.contourArea(contour) > 1000:
+                cv.drawContours(updatedOrangeMask, [contour], 0, 255, -1)
 
-path_x = []
-path_y = []
+    # Update the mask stored in the dictionary
+    mask_dict['orange'] = updatedOrangeMask
 
-y = edy
-x = edx
-# Traces best path
-while True:
-    # x, y starting at the end goes all the way back
-    nbh = dst[y - 1:y + 2, x - 1:x + 2]
-    nbh[1, 1] = 9999999
-    nbh[nbh == 0] = 9999999
-    # If we reach a dead end
-    if np.min(nbh) == 9999999:
-        break
-    idx = np.argmin(nbh)
-    # find direction
-    y += ymesh[idx]
-    x += xmesh[idx]
+    # For the red mask, we are going to remove noise using a different method
+    # in this case, we are applying a morphological operation that removes noise for us
+    kernel = np.ones((5,5), np.uint8)
+    thresh = cv.morphologyEx(mask_dict['red'], cv.MORPH_OPEN, kernel)
 
-    if np.sqrt((x - x1) ** 2 + (y - y1) ** 2) < boxr:
-        print('Optimum route found.')
-        break
-    path_y.append(y)
-    path_x.append(x)
+    # Let us get the contours of this updated threshold, as well as initialize our empty image file
+    contours, hierarchy = cv.findContours(thresh, mode=cv.RETR_LIST, method=cv.CHAIN_APPROX_SIMPLE)
+    updatedRedMask = np.zeros(originalImage.shape[:2], dtype=np.uint8)
+
+    # We are about to find all the polygons within this image, let us initialize an
+    # array that will store these polygons points
+    polygonArr = []
+
+
+    # We will now loop through these contours and draw approximated polygon shapes
+    # for each inch by inch square
+    # We will also add them to our polygon array
+    for i, contour in enumerate(contours):
+        if hierarchy[0][i][2] == -1:
+            approx = cv.approxPolyDP(contour, 0.01*cv.arcLength(contour, True), True)
+            cv.drawContours(updatedRedMask, [approx], 0, (255, 255, 255), 2)
+            polygonArr.append(approx)
+
+    # Update the mask stored in the dictionary
+    mask_dict['red'] = updatedRedMask
+
+    # Return our new masks as well as our polygon array!
+    return mask_dict, polygonArr
+
+
+def findCentroids(mask_dict):
+
+    # This functions purpose is to find the centroids of our green and orange masks
+    # effectively, we are finding the coordinates of where our robot is, and where
+    # it wants to go to!
+
+    # Our output is information on every connected component within this mask
+    # this is done automatically through OpenCV! Even though I would love to
+    # write the DFS for it myself!
+    output = cv.connectedComponentsWithStats(mask_dict['green'], 4, cv.CV_32S)
+    centroids = output[3]
+    stats = output[2]
+
+    # Every connected component also includes the black pixels, which are all connected
+    # Let us find this (by finding the component that is very large) and delete it
+    # as we do not need this.
+    for i in range(len(stats)):
+        if stats[i][4] > 50000:
+            centroids = np.delete(centroids, i, axis=0)
+
+    # This should leave us with only one component left, which is our robots starting point!
+    # Green -> Start || Orange -> End
+    # Ta-da!  <-- Arup Guha Tribute
+    startX, startY = int(centroids[0][0]), int(centroids[0][1])
+
+    # Let us repeat this process for our orange mask to find the end coordinates.
+    output = cv.connectedComponentsWithStats(mask_dict['orange'], 4, cv.CV_32S)
+    centroids = output[3]
+    stats = output[2]
+
+    for i in range(len(stats)):
+        if stats[i][4] > 50000:
+            centroids = np.delete(centroids, i, axis=0)
+
+    # And here are our end coordinates!
+    endX, endY = int(centroids[0][0]), int(centroids[0][1])
+
+    # Let us return these values!
+    return startX, startY, endX, endY
+
+
+def findPaths(mask_dict):
+
+    # Threshold this image to prep to send it to skeletonize
+    if mask_dict['blue'].shape.__len__() > 2:
+        thr_img = (mask_dict['blue'][:, :, 0] > np.max(mask_dict['blue'][:, :, 0])/2)
+    else:
+        thr_img = mask_dict['blue'] > np.max(mask_dict['blue']) / 2
+
+    # Skeletonize the inside of this, which will be our paths
+    skeleton = skeletonize(thr_img)
+
+    # Map of routes
+    mapRoutes = ~skeleton
+
+    # We can show these routes here
+    plt.imshow(mapRoutes)
+    plt.show()
+
+    # Return these routes
+    return mapRoutes, thr_img
+
+def findShortestPath(startX, startY, endX, endY, mapRoutes, thresh):
+
+    # We have our start and end points, but they may not necessarily fall onto
+    # a path of routes, we must use a radius to find the closest equivalence to this
+    # path. Usually it is not that far off, but this value may cause errors, so
+    # be weary of this!
+    boxr = 100
+
+    # Safety check to make sure the points are not out of the image given this radius
+    if endY < boxr: endY = boxr
+    if endX < boxr: endX = boxr
+
+    # Here are our points
+    cpys, cpxs = np.where(mapRoutes[endY - boxr:endY + boxr, endX - boxr:endX + boxr] == 0)
+
+    # Calibrate points to main scale.
+    cpys += endY - boxr
+    cpxs += endX - boxr
+
+    # Find the closest point of possible path end points
+    idx = np.argmin(np.sqrt((cpys - endY) ** 2 + (cpxs - endX) ** 2))
+    x, y = cpxs[idx], cpys[idx]
+
+    pts_x = [x]
+    pts_y = [y]
+    pts_c = [0]
+
+    # Mesh of displacements
+    xmesh, ymesh = np.meshgrid(np.arange(-1, 2), np.arange(-1, 2))
+    ymesh = ymesh.reshape(-1)
+    xmesh = xmesh.reshape(-1)
+
+    dst = np.zeros(thresh.shape)
+
+    # Breath first algorithm exploring a tree
+    while True:
+        # update distance.
+        idc = np.argmin(pts_c)
+        ct = pts_c.pop(idc)
+        x = pts_x.pop(idc)
+        y = pts_y.pop(idc)
+        # Search 3x3 neighbourhood for possible
+        ys, xs = np.where(mapRoutes[y - 1:y + 2, x - 1:x + 2] == 0)
+        # Invalidate these point from future searchers.
+        mapRoutes[ys + y - 1, xs + x - 1] = ct
+        mapRoutes[y, x] = 9999999
+        # set the distance in the distance image.
+        dst[ys + y - 1, xs + x - 1] = ct + 1
+        # extend our list.s
+        pts_x.extend(xs + x - 1)
+        pts_y.extend(ys + y - 1)
+        pts_c.extend([ct + 1] * xs.shape[0])
+        # If we run of points.
+        if not pts_x:
+            break
+        if np.sqrt((x - startX) ** 2 + (y - startY) ** 2) < boxr:
+            edx = x
+            edy = y
+            break
+
+    path_x = []
+    path_y = []
+
+    x = edx
+    y = edy
+
+    # Trace the best path back
+    while True:
+        # x, y starting at the end goes all the way back
+        nbh = dst[y - 1:y + 2, x - 1:x + 2]
+        nbh[1, 1] = 9999999
+        nbh[nbh == 0] = 9999999
+        # If we reach a dead end
+        if np.min(nbh) == 9999999:
+            break
+        idx = np.argmin(nbh)
+        # find direction
+        x += xmesh[idx]
+        y += ymesh[idx]
+
+        # Here is where radius errors can occur
+        if np.sqrt((x - endX) ** 2 + (y - endY) ** 2) < boxr:
+            print('Optimum route found.')
+            break
+        path_x.append(x)
+        path_y.append(y)
+
+    # Return our path!
+    return path_x, path_y
+
+imageArray = importImage('maze8.jpg')
+
+originalImage = imageArray[0]
+hsv_points = imageArray[1]
+
+mask_dict = createMasks(hsv_points, originalImage)
+mask_dict, polygonArr = contourMasks(mask_dict, originalImage)
+
+startX, startY, endX, endY = findCentroids(mask_dict)
+
+mapRoutes, thresh = findPaths(mask_dict)
+path_x, path_y = findShortestPath(startX, startY, endX, endY, mapRoutes, thresh)
 
 plt.figure(figsize=(14,14))
-plt.imshow(rgb_img)
+plt.imshow(originalImage)
 plt.plot(path_x, path_y, 'r-', linewidth=5)
 plt.show()
